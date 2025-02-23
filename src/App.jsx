582cfd391928b05a06c3984ff6f0b3e63f09584c
Somesh Ghosh <someshghoshjoyguru@gmail.com>
2025-02-24 00:38:40 +0530

    first change

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,15 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1>Dr. Rajiv Bhushan</h1>
+      {/* <div className="card">
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Your professional coach for career growth and personal development
         </p>
-      </div>
+      </div> */}
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        {/* Click on the Vite and React logos to learn more */}
       </p>
     </>
   )
